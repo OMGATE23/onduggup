625e5d4fc54bd6365be8276dc9b884b49eccfae6
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-03-08 00:12:06 +0530

    feat : dropdown menu
    Signed-off-by: Om Gate <90276220+OMGATE23@users.noreply.github.com>

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,10 @@
 <script>
 	import { userData } from '../data/user_data';
+	let showMenu = false;
+
+	function toggleMenu() {
+		showMenu = !showMenu;
+	}
 </script>
 
 <header
@@ -7,7 +12,7 @@
 >
 	<a aria-label="duggup-logo" href="/"><img alt="logo" src="/assets/logo.svg" /></a>
 	<div class="flex items-center gap-12">
-		<ul class="flex items-center font-[300] justify-evenly gap-12 text-neutral-800">
+		<ul class="hidden md:flex items-center font-[300] justify-evenly gap-12 text-neutral-800">
 			<li>
 				<a class="flex items-center gap-2" href="/">
 					<img alt="home" src="/assets/home.svg" />
@@ -43,5 +48,37 @@
 			<p class=" text-neutral-800 font-[450] text-xl">{userData.name}</p>
 			<img alt="chevron down" src="/assets/chevron-down.svg" />
 		</button>
+		<button on:click={toggleMenu} class="block md:hidden" aria-label="dropdown menu">
+			<img width="24" alt="menu" src="/assets/hamburger.svg" />
+		</button>
+		{#if showMenu}
+			<div
+				class="fixed flex md:hidden justify-center items-center z-[100] top-0 bg-white left-0 w-[100vw] h-[100dvh]"
+			>
+				<ul class="flex flex-col items-center font-[300] justify-evenly gap-12 text-neutral-800">
+					<li>
+						<a class="flex items-center gap-2 py-1 border-b-[1px] border-neutral-800" href="/">
+							<img alt="home" src="/assets/home.svg" />
+							Home
+						</a>
+					</li>
+					<li>
+						<a class="flex items-center gap-2 py-1 border-b-[1px] border-neutral-800" href="/">
+							<img alt="home" src="/assets/explore.svg" />
+							Explore
+						</a>
+					</li>
+					<li>
+						<a class="flex items-center gap-2 py-1 border-b-[1px] border-neutral-800" href="/">
+							<img alt="home" src="/assets/learn.svg" />
+							Learn
+						</a>
+					</li>
+				</ul>
+				<button on:click={toggleMenu} class="absolute top-4 right-4 z-[120]">
+					<img width="24" alt="close" src="/assets/close.svg" />
+				</button>
+			</div>
+		{/if}
 	</div>
 </header>
