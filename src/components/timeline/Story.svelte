a730c560a5850c4b45d27a05c6ea95773eb2bede
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-03-08 00:13:43 +0530

    style : changed stories flexbox orientation
    Signed-off-by: Om Gate <90276220+OMGATE23@users.noreply.github.com>

--- src/components/timeline/Story.svelte
+++ src/components/timeline/Story.svelte
@@ -16,16 +16,18 @@
 		src="/assets/stories-bar.svg"
 	/>
 	<div
-		class="min-w-[80%] absolute left-12 top-[50%] move-up flex items-center gap-8 p-2 rounded-lg h-full"
+		class=" w-full md:min-w-[80%] absolute left-0 md:left-12 top-[50%] move-up flex flex-col md:flex-row items-center gap-2 md:gap-8 p-2 rounded-lg h-full"
 	>
 		{#each content as story (story.title)}
 			<div
-				class="bg-white w-[50%] outline outline-[0.5px] outline-[#D0D5DD] h-[80%] rounded-lg overflow-hidden"
+				class="bg-white w-full md:w-[50%] md:h-[80%] outline outline-[0.5px] outline-[#D0D5DD] rounded-lg overflow-hidden"
 			>
 				<div class="h-[60%] overflow-hidden">
 					<img class="object-cover w-full" alt={story.title} src={story.thumbnail} />
 				</div>
-				<div class="text-sm p-4 font-[300] flex justify-center items-center text-[#141618]">
+				<div
+					class="text-sm py-1 px-4 md:p-4 font-[300] flex justify-center items-center text-[#141618]"
+				>
 					<p class="clamp-2-lines">{story.title}</p>
 				</div>
 			</div>
