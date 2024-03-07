cfcf5cb8bd16725e32fd71941c3a87e8a6eb163c
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-03-07 21:41:09 +0530

    feat : user introduction component
    Signed-off-by: Om Gate <90276220+OMGATE23@users.noreply.github.com>

---
+++ src/components/UserIntroduction.svelte
@@ -0,0 +1,40 @@
+<script>
+	import { userData } from '../data/user_data';
+	console.log(userData);
+</script>
+
+<div class="flex w-[80%] mx-auto my-12 gap-16">
+	<div class="flex flex-col gap-4 items-center">
+		<img
+			width="120"
+			class="outline outline-[5px] outline-[#EAEFF5] rounded-full"
+			alt={userData.name}
+			src={userData.profile_img}
+		/>
+		<p class="text-2xl text-center text-[#4B5C6D] font-[600]">{userData.name}</p>
+	</div>
+	<div class="flex flex-col gap-8">
+		<div class="flex justify-between gap-20">
+			<p class="w-[75%] text-[#141618] leading-7 space">{userData.description}</p>
+			<div class="flex flex-col items-end">
+				<img class="w-10 rounded-full" alt={userData.company} src={userData.company_img} />
+				<p class="font-[600]">{userData.company}</p>
+				<p class="text-[#7A9299] text-xs">{userData.position}</p>
+			</div>
+		</div>
+		<div class="flex justify-between">
+			<button
+				class="flex items-center gap-2 outline outline-[0.5px] text-[#4D4D4D] px-5 py-1 rounded-[4px] border-b-4 border-[#4D4D4D]"
+				href="/"
+			>
+				Followed
+			</button>
+			<div class="flex items-center gap-4">
+				<a class="flex items-center gap-2" href="/"
+					>My website <img alt="my website" src="/assets/external-link.svg" /></a
+				>
+				<button aria-label="options"><img alt="options" src="/assets/three-dots.svg" /></button>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ src/data/user_data.js
@@ -0,0 +1,58 @@
+export const userData = {
+	profile_img: '/assets/profile-pic-duggup.jpeg',
+	name: 'Krishna Kiran',
+	description:
+		'Co-Founder and CEO at Duggup - Social network for people in tech. Ex-Amazon Head of Engineering. I write hot-takes on building a business, shipping delightful products and accelerating product and career growth.',
+	company: 'Duggup',
+	company_img: '/assets/duggup.png',
+	position: 'Co-Founder and CEO',
+	timeline: [
+		{
+			type: 'stories',
+			date: 'Dec 2023',
+			content: [
+				{
+					title: 'No amount of technology can convert a bad story into a good story.',
+					thumbnail: '/assets/content-img-1.jpeg'
+				},
+				{
+					title:
+						"Most people don't have original ideas. Here is how Sam Altman pushes himself to have unpopular ideas.",
+					thumbnail: '/assets/content-img-2.jpeg'
+				}
+			]
+		},
+		{
+			type: 'job_status',
+			date: 'Nov 2023',
+			company: 'Duggup',
+			company_img: 'duggup.png',
+			location: 'San Francisco Bay Area',
+			title: 'Co-Founder and CEO',
+			job_style: ['Full-time', 'Remote']
+		},
+		{
+			type: 'stories',
+			date: 'Dec 2023',
+			content: [
+				{
+					title: `Startup Lesson I am reflecting. Don't build for the "average person".`,
+					thumbnail: '/assets/content-img-3.jpeg'
+				},
+				{
+					title: 'Your biggest regrets at 80 will be acts of omission.',
+					thumbnail: '/assets/content-img-4.jpeg'
+				}
+			]
+		},
+		{
+			type: 'job_status',
+			date: 'Sep 2022',
+			company: 'Betterup',
+			company_img: 'betterup.jpeg',
+			location: 'San Francisco Bay Area',
+			title: 'VP Engineering',
+			job_style: ['Full-time']
+		}
+	]
+};
